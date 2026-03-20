d3b546deb54da579f72a328fc009473d1d55de0c
GitWithDudi <dudi.gitar@gmail.com>
2026-03-20 03:35:21 +0200

    update helth befor not limit neondb

--- src/Controller/Controller_health.py
+++ src/Controller/Controller_health.py
@@ -1,8 +1,5 @@
-from src.Model.Model_helth import health_check
+
 
 def health_controller():
-    try:
-        row = health_check()
-        return {"status": "ok", "db": "connected", "row": row}, 200
-    except:
-        return {"status": "ok", "db": "waking up"}, 200
+    
+    return {"status": "ok"}, 200
--- src/Model/Model_helth.py
+++ src/Model/Model_helth.py
@@ -1,8 +1,8 @@
-from src.Utils.db_config import get_db_connection
+# from src.Utils.db_config import get_db_connection
 
-def health_check():
-     with get_db_connection() as conn:
-        cur = conn.cursor()
-        cur.execute("SELECT 1 FROM portfolio.health LIMIT 1")
-        result = cur.fetchone()
-        return result
+# def health_check():
+#      with get_db_connection() as conn:
+#         cur = conn.cursor()
+#         cur.execute("SELECT 1 FROM portfolio.health LIMIT 1")
+#         result = cur.fetchone()
+#         return result
